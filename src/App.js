13648cb3d610800fcae3fc734602ab8990522665
Laura Badgley <l.badgleyy@gmail.com>
2019-08-21 13:36:22 -0700

    designing header with links scalability

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import NavBar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
-    
+    super(props);
+    this.state = {
+      title: 'Laura Badgley',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact Me', path: '/contact' }
+      ],
+      home: {
+        title: 'Hi, I\'m Laura',
+        subTitle: 'Fun projects I\'ve made',
+        smallDescriptionText: 'Check out my projects below',
+      },
+      about: {
+        title: 'About Me',
+      },
+      contact: {
+        title: 'Let\'s Talk',
+      }
+    }
   }
 
 
   render () {
     return (
-      <div>Hello from React!</div>
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <NavBar className="border-bottom" bg="transparent" expand="lg">
+            <NavBar.Brand>Laura Badgley</NavBar.Brand>
+            <NavBar.Toggle aria-controls="navbar-toggle" />
+
+            <NavBar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-Link" to="/Home">Home</Link>
+                <Link className="nav-Link" to="/About">About</Link>
+                <Link className="nav-Link" to="/Contact">Contact</Link>
+              </Nav>
+            </NavBar.Collapse>
+          </NavBar>
+        </Container>
+      </Router>
     );
   }
 }
